f84e4080bf5181c961e58cf1437738546b9ff39d
Josh Dechant <josh@smarterwebdev.com>
2009-04-16 22:01:56 +0000

    implemented interface methods on Willow_Validate
    git-svn-id: http://subversion.assembla.com/svn/phpwillow/trunk@6 0fdf18b5-fe09-4a94-8c34-9a0b2b97716d

--- Framework/Vendor/Willow/Plugin/Core/Validate.plugin/Willow/Validate.php
+++ Framework/Vendor/Willow/Plugin/Core/Validate.plugin/Willow/Validate.php
@@ -44,4 +44,68 @@ class Willow_Validate implements Willow_Registerable_Interface,
         }
     }
 
+    /**
+     * ...
+     */
+    protected static $_classMap = array();
+
+    /**
+     * Register a class under an alias
+     *
+     * @param string $alias
+     * @param mixed $class
+     * @return void
+     */
+    public static function register($alias, $class)
+    {
+        self::$_classMap[$alias] = $class;
+    }
+
+    /**
+     * Get instance of class registered under given alias
+     *
+     * @param string $alias
+     * @return object
+     * @throws Exception
+     */
+    public function getRegistered($alias)
+    {
+        /**
+         * Transient alias is registered
+         */
+        if (array_key_exists($alias, $this->_transientMap) === true)
+        {
+            return $this->_transientMap[$alias];
+        }
+
+        /**
+         * Alias is registered
+         */
+        elseif (array_key_exists($alias, self::$_classMap))
+        {
+            return self::$_classMap[$alias];
+        }
+
+        throw new Willow_Validate_Exception(sprintf(
+            'Use of unregistered Willow_Validate alias, "%s"', $alias
+        ));
+    }
+
+    /**
+     * ...
+     */
+    protected $_transientMap = array();
+
+    /**
+     * Register a class under an alias for this instance
+     *
+     * @param string $alias
+     * @param mixed $class
+     * @return self
+     */
+    public function registerTransient($alias, $class)
+    {
+        $this->_transientMap[$alias] = $class;
+    }
+
 }
