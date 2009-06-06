d425a7e2a875abd057bf675a17f62de885f36fa4
Josh Dechant <josh@smarterwebdev.com>
2009-06-06 16:21:06 +0000

    Add generic Willow_Data_Object
    git-svn-id: http://subversion.assembla.com/svn/phpwillow/trunk@33 0fdf18b5-fe09-4a94-8c34-9a0b2b97716d

---
+++ Framework/Vendor/Willow/Willow/Data/Object.php
@@ -0,0 +1,143 @@
+<?php
+/* $Id$ */
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+
+class Willow_Data_Object implements ArrayAccess, IteratorAggregate
+{
+
+    /**
+     * ...
+     */
+    protected $_properties = array();
+
+    /**
+     * ...
+     */
+    public function __construct(array $properties = array())
+    {
+        if (count($properties) > 0)
+        {
+            $this->import($properties);
+        }
+    }
+
+    /**
+     * ...
+     */
+    public function import(array $properties)
+    {
+        foreach ($properties as $property => $value)
+        {
+            $this->set($property, $value);
+        }
+    }
+
+    /**
+     * ...
+     */
+    public function get($property)
+    {
+        if (isset($this->$property) === true)
+        {
+            return $this->_properties[$property];
+        }
+    }
+
+    /**
+     * ...
+     */
+    public function set($property, $value)
+    {
+        $this->_properties[$property] = $value;
+    }
+
+    /**
+     * ...
+     */
+    public function isValid($property)
+    {
+        return array_key_exists($property, $this->_properties);
+    }
+
+    /**
+     * ...
+     */
+    public function remove($property)
+    {
+        unset($this->_properties[$property]);
+    }
+
+    /**
+     * For IteratorAggregate
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->_properties);
+    }
+
+    /**
+     * For ArrayAccess
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * For ArrayAccess
+     */
+    public function offsetSet($offset, $value)
+    {
+        return $this->set($value);
+    }
+
+    /**
+     * For ArrayAccess
+     */
+    public function offsetExists($offset)
+    {
+        return $this->isValie($offset);
+    }
+
+    /**
+     * For ArrayAccess
+     */
+    public function offsetUnset($offset)
+    {
+        return $this->remove($offset);
+    }
+
+    /**
+     * ...
+     */
+    public function __get($property)
+    {
+        return $this->get($property);
+    }
+
+    /**
+     * ...
+     */
+    public function __set($property, $value)
+    {
+        return $this->set($property);
+    }
+
+    /**
+     * ...
+     */
+    public function __isset($property)
+    {
+        return $this->isValie($property);
+    }
+
+    /**
+     * ...
+     */
+    public function __unset($property)
+    {
+        return $this->remove($property);
+    }
+
+}
