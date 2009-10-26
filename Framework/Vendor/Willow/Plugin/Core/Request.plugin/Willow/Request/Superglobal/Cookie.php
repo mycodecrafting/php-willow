9b85645e4edbe6d0ccd2a6a45298ddd8fc673449
Josh Dechant <josh@smarterwebdev.com>
2009-10-26 04:13:56 +0000

    Added set() and delete() methods to Willow_Request_Superglobal_Cookie
    git-svn-id: http://subversion.assembla.com/svn/phpwillow/trunk@66 0fdf18b5-fe09-4a94-8c34-9a0b2b97716d

--- Framework/Vendor/Willow/Plugin/Core/Request.plugin/Willow/Request/Superglobal/Cookie.php
+++ Framework/Vendor/Willow/Plugin/Core/Request.plugin/Willow/Request/Superglobal/Cookie.php
@@ -14,4 +14,24 @@ class Willow_Request_Superglobal_Cookie extends Willow_Request_Abstract
         $this->import($_COOKIE);
     }
 
+    /**
+     * ...
+     */
+    public function set($property, $value, $expires = 0, $path = '/')
+    {
+        $this->$property = $value;
+        setcookie($property, $value, strtotime($expires), $path);
+        return $this;
+    }
+
+    /**
+     * ...
+     */
+    public function delete($property, $path = '/')
+    {
+        $this->set($property, '', '-30 Days');
+        unset($this->$property);
+        return $this;
+    }
+
 }
