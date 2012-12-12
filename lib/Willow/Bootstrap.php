21de44336456459f3ad2644303e607f597df5857
Joshua Dechant <josh@smarterwebdev.com>
2012-12-12 15:52:09 -0500

    updating error_reporting flag

--- lib/Willow/Bootstrap.php
+++ lib/Willow/Bootstrap.php
@@ -6,7 +6,7 @@
 /**
  * Setup error reporting
  */
-error_reporting(E_ALL | E_STRICT);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 
